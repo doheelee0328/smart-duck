826906f61f9547d2f476ee413066a411f9dc8da1
Dohee Lee <dojuhee2015@gmail.com>
2023-10-12 23:52:57 +0100

    add animation

--- client/src/pages/homepage/homepage.tsx
+++ client/src/pages/homepage/homepage.tsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import Arrow from '../../images/Arrow.png'
+import Duck from '../../images/Duck.png'
 import '../../scss/main.scss'
 
 const homepage = () => {
-  return <div className='homepage-background'>homepage</div>
+  return (
+    <div className='homepage-background'>
+      <div className='smart-duck-container'>
+        <h1 className='welcome-message'>Welcome To SmartDuck</h1>
+
+        <p className='begin-learning-message'>
+          Click the duck to begin your learning
+        </p>
+        <div className='image-container'>
+          <img src={Arrow} alt='arrow' className='arrow-image' />
+          <img src={Duck} alt='duck' className='duck-image' />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default homepage
