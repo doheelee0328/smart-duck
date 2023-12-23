f5b1529964afd2bc25ad62d439fd909a313bcfaf
Dohee Lee <dojuhee2015@gmail.com>
2023-12-23 22:10:37 +0000

    added character

--- client/src/pages/character/Character.tsx
+++ client/src/pages/character/Character.tsx
@@ -1,8 +1,23 @@
 import React from 'react'
 import '../../scss/main.scss'
+import Duck1 from '../../images/duck1.png'
+
+interface CharacterProps {
+  image: any
+  alt: string
+}
+
+const CharacterImages = ({ image, alt }: CharacterProps) => (
+  <img src={image} alt={alt} className='duck-image' />
+)
 
 const Character = () => {
-  return <div className=''>character</div>
+  return (
+    <div className='character-container'>
+      <h1 className='character-heading'>Choose Your Duck</h1>
+      <CharacterImages image={Duck1} alt='duck1' />
+    </div>
+  )
 }
 
 export default Character
