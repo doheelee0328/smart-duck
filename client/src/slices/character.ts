af4e143824a44bdddce6a744847b74ac67818488
Dohee Lee <dojuhee2015@gmail.com>
2023-12-24 10:56:31 +0000

    add slice

--- client/src/slices/character.ts
+++ client/src/slices/character.ts
@@ -1,1 +1,19 @@
 import { createSlice } from '@reduxjs/toolkit'
+
+type CharacterState = {
+  character: any
+}
+
+const initialCounterState: CharacterState = {
+  character: null,
+}
+
+const characterSlice = createSlice({
+  name: 'character',
+  initialState: initialCounterState,
+  reducers: {
+    // changeCharacter(state) {
+    //   state.character =
+    // }
+  },
+})
